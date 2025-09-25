1d13cacb8b99b06c8e56eeb3456492df9d44a78b
billchiles <bill_chiles@hotmail.com>
2025-09-24 22:40:35 -0700

    removing debugging preload.cjs, forgot before in previous commit.

--- electron/preload.cjs
+++
@@ -1,9 +0,0 @@
-// electron/preload.cjs  (CommonJS preload)
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("electron", {
-  isElectron: true,
-  ping: () => ipcRenderer.invoke("ping"),
-
-  // (we can add the file APIs back after the base case works)
-});
